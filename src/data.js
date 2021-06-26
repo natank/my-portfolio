2bc314a4bf3dd5eff377cea37348d666d4ff5dd1
Nati Kamusher <natan.kamusher@gmail.com>
2021-06-26 19:42:04 +0300

    Added travel.gif

--- src/data.js
+++ src/data.js
@@ -4,8 +4,8 @@ export const projects = [
 		subtitle: 'MERN Stack',
 		description:
 			'Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?',
-		image: './project-1.gif',
-		link: 'https://reactbootcamp.com',
+		image: './travel.gif',
+		link: 'https://natank.github.io/Travel-Site',
 	},
 	{
 		title: 'React Tracks',
